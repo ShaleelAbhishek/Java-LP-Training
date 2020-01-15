0083214f43bbdc41747b98a6981e3961fea3ca6b
shaleel <shaleelca@gmail.com>
2020-01-15 13:32:32 +0530

    updated star pattern

---
+++ Star_Pattern/StarPattern.java
@@ -0,0 +1,38 @@
+/**********Star Pattern***********/
+/**********14.01.2019*************/
+/*******Shaleel Abhishek**********/
+
+import java.util.Scanner;
+
+
+/********************************/
+
+
+public class StarPattern {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number of Rows : ");
+        int rows = scanner.nextInt();
+
+            for (int a=1; a<=rows; a++){			//give condition to print enterd number of rows
+                 for (int b=rows; b>a; b--){
+                        System.out.print(" ");
+                 }
+                 for (int c=1; c<=(a * 2) -1; c++){		//stars should be odd number everytime. so (number*2)-1 gives 
+                         System.out.print("*");			//an odd number.
+                 }
+                System.out.println();
+             }
+
+            for (int a=rows-1; a>=1; a--){
+                 for (int b=rows-1; b>=a; b--){
+                     System.out.print(" ");
+                 }
+                 for (int c=1; c<=(a * 2) -1; c++){
+                     System.out.print("*");
+                 }
+                System.out.println();
+             }
+    }
+}
