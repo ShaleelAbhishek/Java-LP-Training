490e7c3f0308dd57cdbf38d636faa5afbf872186
shaleel <shaleelca@gmail.com>
2020-01-20 23:38:27 +0530

    updated lambdaEx Example with all

---
+++ LambdaExpression_Simple/Calc.java
@@ -0,0 +1,10 @@
+/****Simple Lambda Expression*****/
+/**********20.01.2019*************/
+/*******Shaleel Abhishek**********/
+
+
+public interface Calc {
+    int power(int x);
+    
+}
+
---
+++ LambdaExpression_Simple/LambdaExpression.java
@@ -0,0 +1,19 @@
+/****Simple Lambda Expression*****/
+/**********20.01.2019*************/
+/*******Shaleel Abhishek**********/
+
+
+public class LambdaExpression{
+	public static void main(String[] args){
+
+ 
+	Calc c = x -> x*x;
+	int Power=c.power(10);
+	
+	System.out.println(Power);
+
+  }
+}
+
+
+
