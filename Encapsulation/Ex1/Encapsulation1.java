2ab5ed1d8dc07134a8584624c687202c9e4d90e5
shaleel <shaleelca@gmail.com>
2020-01-20 21:47:55 +0530

    updated Encapsulation Example with all

---
+++ Encapsulation/Ex1/Encapsulation1.java
@@ -0,0 +1,17 @@
+/**********Encapsulation**********/
+/**********14.01.2019*************/
+/*******Shaleel Abhishek**********/
+
+public class Encapsulation1 {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.setName("Abhi");
+        s1.setAge(9);
+        
+        System.out.println(s1.getName());
+        System.out.println(s1.getAge());
+
+    }
+}
---
+++ Encapsulation/Ex1/Student.java
@@ -0,0 +1,23 @@
+/**********Encapsulation**********/
+/**********14.01.2019*************/
+/*******Shaleel Abhishek**********/
+
+public class Student {
+   private String Name;
+   private   int Age;
+
+    public void setName(String newName){
+        Name = newName;
+    }
+    public String getName(){
+        return Name;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+}
